169d5d4f8f3fd0af905aef4fb6bc115cc48b2618
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-01 14:32:24 -0700

    Update challenge.js

--- js/challenge.js
+++ js/challenge.js
@@ -5,33 +5,58 @@
     <title>Interactive Timer</title>
     </head>
 <body>
-  <script>
-    function myFunction() {
-        alert('Hello World!');
-    }
-    function showTime() {
-    var d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-}
-setInterval(showTime, 1000);
+  "use strict";
 
-let intervalID;
- 
-function showTime() {
-    var d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-}
- 
-function stopClock() {
-    clearInterval(intervalID);
-}
- 
-var intervalID = setInterval(showTime, 1000);
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
 
-    </script>
-    <button onclick="setTimeout(myFunction, 2000)">Click Me</button>
-    <p>The current time on your computer is: <span id="clock"></span></p>
-    <p>The current time on your computer is: <span id="clock"></span></p>
-    <button onclick="stopClock();">Stop Clock</button>
+let cron;
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
+}
+
+function pause() {
+  clearInterval(cron);
+}
+
+function reset() {
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+  document.getElementById('hour').innerText = '00';
+  document.getElementById('minute').innerText = '00';
+  document.getElementById('second').innerText = '00';
+  document.getElementById('millisecond').innerText = '000';
+}
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
+  document.getElementById('hour').innerText = returnData(hour);
+  document.getElementById('minute').innerText = returnData(minute);
+  document.getElementById('second').innerText = returnData(second);
+  document.getElementById('millisecond').innerText = returnData(millisecond);
+}
+
+function returnData(input) {
+  return input > 10 ? input : `0${input}`
+}
 </body>
 </html>
